a1441a06a2e911a9cb63ee8109bbdfa486d626ce
Serge Ipatieff <ipatieff@yandex.ru>
2022-02-09 21:28:26 +0300

    main: fix for new version

--- src/components/AHT/AHT.tsx
+++ src/components/AHT/AHT.tsx
@@ -5,7 +5,7 @@ export type ITextAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 const AHT: React.FC<ITextAreaProps> = ({ children, ...props }) => {
   return (
-    <textarea className="aht" ref={(element) => autosize(element)} {...props}>
+    <textarea ref={(element) => autosize(element)} {...props}>
       {children}
     </textarea>
   );
